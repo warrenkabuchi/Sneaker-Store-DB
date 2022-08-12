b2d26b4bcb2dbe6bcd469d44c191c01c5747029a
warrenkabuchi <78430174+warrenkabuchi@users.noreply.github.com>
2022-08-12 08:49:27 -0500

    Initial commit

---
+++ InsertData.sql
@@ -0,0 +1,71 @@
+INSERT INTO `sneaker_store`.`customers`
+(`cust_id`,
+`order_id`,
+`First_Name`,
+`Last_Name`,
+`Address`,
+`City`,
+`State`,
+`Zip`)
+VALUES
+('1',1,'Michael','Jackson','321 Wuhan Blvd','Wuhan','TX',70113);
+
+INSERT INTO `sneaker_store`.`customers`
+(`cust_id`,
+`order_id`,
+`First_Name`,
+`Last_Name`,
+`Address`,
+`City`,
+`State`,
+`Zip`)
+VALUES
+('2',2,'Mikel','Arteta','321 Gunnar Blvd','Wuhan','TX',70113);
+
+INSERT INTO `sneaker_store`.`customers`
+(`cust_id`,
+`order_id`,
+`First_Name`,
+`Last_Name`,
+`Address`,
+`City`,
+`State`,
+`Zip`)
+VALUES
+('3',3,'Pier-Emrik','Aubameyang','321 Gabon Blvd','Gabon','TX',70113);
+
+INSERT INTO `sneaker_store`.`customers`
+(`cust_id`,
+`order_id`,
+`First_Name`,
+`Last_Name`,
+`Address`,
+`City`,
+`State`,
+`Zip`)
+VALUES
+('4',4,'Nicolas','Pepe','321 Fraud Blvd','Werstrure','TX',70113);
+
+INSERT INTO `sneaker_store`.`customers`
+(`cust_id`,
+`order_id`,
+`First_Name`,
+`Last_Name`,
+`Address`,
+`City`,
+`State`,
+`Zip`)
+VALUES
+('1',1,'Michael','Jackson','321 Wuhan Blvd','Wuhan','TX',70113);
+
+INSERT INTO `sneaker_store`.`customers`
+(`cust_id`,
+`order_id`,
+`First_Name`,
+`Last_Name`,
+`Address`,
+`City`,
+`State`,
+`Zip`)
+VALUES
+('1',1,'Sam','Wilson','321 AWS Blvd','Servers','TX',70113);
